dbb6df9f5a6190e090f2d8a7e9587ce34a4bb085
ORBY-ASI <contact@orby-asi.org>
2025-04-01 23:30:23 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Navigation />
       <BackgroundStarlight />
       <Component {...pageProps} />
       <Footer />
