e82ca6724c5dec9a4d1c09a030a139557d185a5b
ORBY-ASI <contact@orby-asi.org>
2025-04-01 23:36:22 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-// pages/_app.tsx
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import BackgroundStarlight from "../components/BackgroundStarlight";
 import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <BackgroundStarlight />
-      <Component {...pageProps} />
       <Navigation />
+      <Component {...pageProps} />
       <Footer />
     </>
   );
